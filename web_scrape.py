88709367c477b927c0c954140038bad92f131671
emilyyiip <144075069+emilyyiip@users.noreply.github.com>
2025-06-24 15:13:30 -0700

    cleaned dataset

--- web_scrape.py
+++ web_scrape.py
@@ -1,5 +1,6 @@
 import requests 
 from bs4 import BeautifulSoup
+import time
 
 base_url = "https://www.notion.com"
 add_url = "https://www.notion.com/help/"
@@ -16,25 +17,29 @@ for a_tag in soup.find_all("a", href = True):
         links.add(full_url)
         full_url = base_url + href.split("?")[0]   #extracts latter portion after base
         links.add(full_url)
+
 #for each link, get content
 articles = []
 for i, url in enumerate(sorted(links)):
     page = requests.get(url, headers = headers)
     soup_i = BeautifulSoup(page.text, "html.parser")
     title = soup_i.find("h1").text.strip() if soup_i.find("h1") else "Untitled"
-    content_block = soup_i.find_all(["h1", "h2", "h3", "p", "li"])
+    content_block = soup_i.find_all(["h2", "h3", "p", "li"])
+    content_cleaned = {text for text in content_block if len(text) > 40}
     content_text = "\n".join([block.get_text(strip=True) for block in content_block])
 
-articles.append({
-    "url": url,
-    "title": title,
-    "content": content_text
-})
+    articles.append({
+        "url": url,
+        "title": title,
+        "content": content_text
+    })
 
+    print(f"[{i+1}/{len(articles)}] Scraped: {title}")
+    time.sleep(1) 
 
-"""
-content = soup.find_all(["h1", "h2", "h3", "li", "p"])
-text_content = "\n".join([block.get_text(strip = True) for block in content])
-with open("grab_text.txt", "w") as f:
-    f.write(text_content)
-"""
+#write all info to text file
+with open("grab_text.txt", "w", encoding="utf-8") as f:
+    for article in articles:
+        f.write(f"### {article['title']}\n")
+        f.write(article['content'])
+        f.write("\n+ --- \n")
